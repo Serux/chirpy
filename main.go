f88d6f12afd9ded361dd19b8324383c365f99ac5
Serux <serchi93@hotmail.com>
2025-01-09 20:05:39 +0100

    chirp len and prof checker

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -17,10 +19,19 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 
 }
-func (cfg *apiConfig) nRequestHandler(rw http.ResponseWriter, _ *http.Request) {
-	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+func (cfg *apiConfig) metricsHandler(rw http.ResponseWriter, _ *http.Request) {
+	rw.Header().Add("Content-Type", "text/html; charset=utf-8")
 	rw.WriteHeader(http.StatusOK)
-	val := fmt.Sprint("Hits: ", cfg.fileserverHits.Load())
+
+	template := `
+	<html>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+	</html>`
+	val := fmt.Sprintf(template, cfg.fileserverHits.Load())
+	//val := fmt.Sprint("Hits: ", cfg.fileserverHits.Load())
 	rw.Write([]byte(val))
 }
 func (cfg *apiConfig) nRequestResetHandler(rw http.ResponseWriter, _ *http.Request) {
@@ -34,12 +45,20 @@ func (cfg *apiConfig) nRequestResetHandler(rw http.ResponseWriter, _ *http.Reque
 func main() {
 	mux := http.NewServeMux()
 	apiConf := apiConfig{}
+
+	//APP FILESERVER
 	appFileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	mux.Handle("/app/", apiConf.middlewareMetricsInc(appFileServerHandler))
-	mux.HandleFunc("GET /metrics", apiConf.nRequestHandler)
-	mux.HandleFunc("POST /reset", apiConf.nRequestResetHandler)
-	mux.HandleFunc("GET /healthz", healthzHandler)
 
+	//API
+	mux.HandleFunc("GET /api/healthz", healthzHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
+
+	//ADMIN
+	mux.HandleFunc("GET /admin/metrics", apiConf.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiConf.nRequestResetHandler)
+
+	//START SERVER
 	server := http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
 }
@@ -48,4 +67,56 @@ func healthzHandler(rw http.ResponseWriter, _ *http.Request) {
 	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	rw.WriteHeader(http.StatusOK)
 	rw.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func validateChirpHandler(rw http.ResponseWriter, r *http.Request) {
+	type requestJson struct {
+		Body string `json:"body"`
+	}
+	type responseJson struct {
+		CleanedBody string `json:"cleaned_Body"`
+	}
+	type errorJson struct {
+		Error string `json:"error"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := requestJson{}
+
+	err := decoder.Decode(&params)
+	if err != nil {
+		// an error will be thrown if the JSON is invalid or has the wrong types
+		// any missing fields will simply have their values in the struct set to their zero value
+		errJ, _ := json.Marshal(errorJson{Error: "Chirp is too long"})
+		json.Marshal(errorJson{Error: "Something went wrong"})
+		rw.WriteHeader(500)
+		rw.Write(errJ)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		errJ, _ := json.Marshal(errorJson{Error: "Chirp is too long"})
+		rw.WriteHeader(400)
+		rw.Write(errJ)
+		return
+	}
+
+	newBody := removeProfanity(params.Body)
+
+	retJ, _ := json.Marshal(responseJson{CleanedBody: newBody})
+	rw.WriteHeader(200)
+	rw.Write(retJ)
+
+}
+
+func removeProfanity(chirp string) string {
+	profanes := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Fields(chirp)
+	for i, w := range words {
+		for _, p := range profanes {
+			if strings.ToLower(w) == p {
+				words[i] = "****"
+			}
+		}
+	}
+	return strings.Join(words, " ")
 }
