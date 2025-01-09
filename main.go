5734b706a3185dfbb89b82bf60caecf85d46dfa2
Serux <serchi93@hotmail.com>
2025-01-09 12:24:05 +0100

    added Metrics

--- main.go
+++ main.go
@@ -1,14 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+
+}
+func (cfg *apiConfig) nRequestHandler(rw http.ResponseWriter, _ *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(http.StatusOK)
+	val := fmt.Sprint("Hits: ", cfg.fileserverHits.Load())
+	rw.Write([]byte(val))
+}
+func (cfg *apiConfig) nRequestResetHandler(rw http.ResponseWriter, _ *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+	val := "Hits Reset."
+	rw.Write([]byte(val))
+}
+
 func main() {
-	serveMux := http.NewServeMux()
+	mux := http.NewServeMux()
+	apiConf := apiConfig{}
+	appFileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiConf.middlewareMetricsInc(appFileServerHandler))
+	mux.HandleFunc("/metrics", apiConf.nRequestHandler)
+	mux.HandleFunc("/reset", apiConf.nRequestResetHandler)
+	mux.HandleFunc("/healthz", healthzHandler)
 
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
-
-	server := http.Server{Handler: serveMux, Addr: ":8080"}
+	server := http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
+}
+
+func healthzHandler(rw http.ResponseWriter, _ *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func add(rw http.ResponseWriter, _ *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte(http.StatusText(http.StatusOK)))
 }
